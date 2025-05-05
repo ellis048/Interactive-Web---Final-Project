bb7c19ec92564d89bf233c88dfca3072c498e726
ellis048 <ellis.megacoolest09@outlook.com>
2025-05-05 16:26:56 -0500

    Major upadate

---
+++ Navbar.js
@@ -0,0 +1,9 @@
+fetch('/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar').innerHTML = data;
+    });
+
+    window.addEventListener('load', function () {
+      document.body.classList.add('loaded');
+    });
