30ecf8cb6146436ec0ab696f5256b385109ae952
ellis048 <ellis.megacoolest09@outlook.com>
2025-05-05 17:07:43 -0500

    nav-bar changes

--- Navbar.js
+++ Navbar.js
@@ -1,9 +1,9 @@
-fetch('/navbar.html')
+fetch('./navbar.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('navbar').innerHTML = data;
     });
 
-    // window.addEventListener('load', function () {
-    //   document.body.classList.add('loaded');
-    // });
+    window.addEventListener('load', function () {
+      document.body.classList.add('loaded');
+    });
