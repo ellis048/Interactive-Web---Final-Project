740a6ea86a85f5a26c39465d8ba233e43a1ecc97
ellis048 <ellis.megacoolest09@outlook.com>
2025-05-15 14:44:21 -0500

    animations and slight changes

---
+++ typedjs/typed-about.js
@@ -0,0 +1,20 @@
+
+    //animations for about.html
+    var typed = new Typed('#aboutus', {
+          strings: ['About Us'],
+          typeSpeed: 100,
+          startDelay: 100,
+          showCursor: false
+        });
+    var typed = new Typed('#mission', {
+          strings: ['Our Mission'],
+          typeSpeed: 100,
+          startDelay: 200,
+          showCursor: false
+        });
+    var typed = new Typed('#WhoWeAre', {
+          strings: ['Who We Are'],
+          typeSpeed: 100,
+          startDelay: 340,
+          showCursor: false
+        });
---
+++ typedjs/typed-index.js
@@ -0,0 +1,25 @@
+    //animations for index.html 
+    var typed = new Typed('#experience', {
+      strings: ['Experience the Future'],
+      typeSpeed: 45,
+      startDelay: 100,
+      showCursor: false
+    });
+    var typed = new Typed('#discover', {
+      strings: ['Discover the next generation of MageOLED. Sleek design. Powerful performance. Made for you.'],
+      typeSpeed: 20,
+      startDelay: 100,
+      showCursor: false
+    });
+    var typed = new Typed('#stunning',{
+        strings: ['Stunning Display'],
+        typeSpeed: 40,
+        startDelay: 100, 
+        showCursor:false
+    });
+    var typed = new Typed('#immerse',{
+        strings: ['Immerse yourself in rich colors and sharp details with our edge-to-edge OLED display. Designed to be both beautiful and functional.'],
+        typeSpeed: 10,
+        startDelay: 100, 
+        showCursor:false
+    });
