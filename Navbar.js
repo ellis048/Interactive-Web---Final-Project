2dfb4907c6819109cd10feb8f1761c543ce51001
ellis048 <ellis.megacoolest09@outlook.com>
2025-05-15 15:28:25 -0500

    fast change and fix,  removing parent directories due to stactic site

--- Navbar.js
+++ Navbar.js
@@ -1,8 +1,8 @@
-fetch('../navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navbar').innerHTML = data;
-    });
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+  });
 
     window.addEventListener('load', function () {
       document.body.classList.add('loaded');
